248fa6ded68ab2ea91bcdce573ee28a138aa9292
DmitriiGudin <49461403+DmitriiGudin@users.noreply.github.com>
2022-12-05 17:37:52 -0500

    Add files via upload

--- seqGen/examples.py
+++ seqGen/examples.py
@@ -8,8 +8,11 @@ from seqGen import generate_a3m
 
 if __name__ == "__main__":
     
-    print ("Generating an *.a3m-file of words of length 20 altered by word removal and insertion: 10 sequences, 'amino' alphabet...")
-    generate_a3m ('words_generated', 20, 10)
+    print ("Generating an *.a3m-file of words of length 32 altered by word removal and insertion: 128 sequences, 'amino' alphabet...")
+    generate_a3m ('words_generated_train', 32, 128)
+    
+    print ("Generating an *.a3m-file of words of length 32 altered by word removal and insertion: 128 sequences, 'amino' alphabet...")
+    generate_a3m ('words_generated_test', 32, 128)
     
     print ("Generating an *.a3m-file with the specified original sentence altered by word removal and insertion: 15 sequences, 'full' alphabet...")
     sentence = 'THE ONLY REAL PRISON IS FEAR AND THE ONLY REAL FREEDOM IS FREEDOM FROM FEAR'
